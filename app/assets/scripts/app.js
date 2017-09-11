51f14cecff3271f4fc2e264f51b6861cbe2a1150
Roman Yosypenko <yosypenkoif@gmail.com>
2017-09-11 21:13:39 +0300

    Add lightSlider

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,3 +2,16 @@ $('.site-header__menu-icon').on('click', function() {
   $(this).toggleClass('site-header__menu-icon--close-x');
   $('.primary-nav').slideToggle(300);
 });
+
+$(document).ready(function() {
+  $("#lightSlider").lightSlider({
+    pager: false,
+    auto: true,
+    item: 1,
+    loop: true,
+    slideMove: 1,
+    easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    speed: 800,
+    pause: 3000
+   });
+ });

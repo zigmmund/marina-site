02d067bb186cd0ac625165e1b2ecbd65b13458b8
Roman Yosypenko <yosypenkoif@gmail.com>
2017-09-18 19:38:22 +0300

    Update google map section, new build

---
+++ app/assets/scripts/map.js
@@ -0,0 +1,137 @@
+function initMap() {
+  var uluru = {lat: 37.076777, lng: -8.117668};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    styles: [
+              {
+                  "featureType": "administrative",
+                  "elementType": "all",
+                  "stylers": [
+                      {
+                          "color": "#ff0000"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "administrative",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                      {
+                          "color": "#474683"
+                      },
+                      {
+                          "weight": "1"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "administrative",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [
+                      {
+                          "color": "#ffffff"
+                      },
+                      {
+                          "lightness": "50"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "landscape",
+                  "elementType": "all",
+                  "stylers": [
+                      {
+                          "color": "#f2f2f2"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "landscape",
+                  "elementType": "geometry.fill",
+                  "stylers": [
+                      {
+                          "color": "#d9d8e5"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "poi",
+                  "elementType": "all",
+                  "stylers": [
+                      {
+                          "visibility": "off"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "road",
+                  "elementType": "all",
+                  "stylers": [
+                      {
+                          "saturation": -100
+                      },
+                      {
+                          "lightness": 45
+                      }
+                  ]
+              },
+              {
+                  "featureType": "road",
+                  "elementType": "geometry.fill",
+                  "stylers": [
+                      {
+                          "color": "#ffffff"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "road.highway",
+                  "elementType": "all",
+                  "stylers": [
+                      {
+                          "visibility": "simplified"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "road.arterial",
+                  "elementType": "labels.icon",
+                  "stylers": [
+                      {
+                          "visibility": "off"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "transit",
+                  "elementType": "all",
+                  "stylers": [
+                      {
+                          "visibility": "off"
+                      }
+                  ]
+              },
+              {
+                  "featureType": "water",
+                  "elementType": "all",
+                  "stylers": [
+                      {
+                          "color": "#474683"
+                      },
+                      {
+                          "visibility": "on"
+                      }
+                  ]
+              }
+          ],
+    center: uluru,
+    disableDefaultUI: true
+  });
+  var iconBase = 'assets/images/custom-pin.svg';
+
+  var marker = new google.maps.Marker({
+    icon: iconBase,
+    position: uluru,
+    map: map
+  });
+}
